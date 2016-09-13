f94c0aa4dab3c96154d4ad816ee634c91608402d
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-13 17:27:48 +0000

    refactored directory.rb

--- directory.rb
+++ directory.rb
@@ -1,19 +1,27 @@
-# First, we print the list of students
-puts "The students of Villains Academy"
-puts "-----------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddie Kruger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# Then we print the total number of students. The lack of a newline function 
-# for print means we don't have to change the number 11 to a string.
-print "Overall we have "
-print 11
-print " great students."
+
+
+# Storing the students in an array allows us to easily use the entries for
+# multiple purposes
+students=[ "Dr. Hannibal Lecter",
+           "Darth Vader",
+           "Nurse Ratched",
+           "Michael Corleone",
+           "Alex DeLarge",
+           "The Wicked Witch of the West",
+           "Terminator",
+           "Freddie Kruger",
+           "The Joker",
+           "Joffrey Baratheon",
+           "Norman Bates"
+         ]
+
+# This prevents us from having to manually update the student count
+student_count=students.count
+
+# This string covers 2 lines.
+students_intro = "The students of Villains Academy\n-----------------"
+
+# THis set of puts gives us a nice set of info on the students
+puts students_intro
+puts students[0..-1]
+puts "\nOverall we have #{student_count} great students."
