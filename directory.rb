c2e8f879a425ace778afc0ced2afc9e9af8be0d3
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-14 10:10:14 +0000

    added .each_with_index to puts_students method

--- directory.rb
+++ directory.rb
@@ -1,36 +1,65 @@
-students=[ 
-           {name: "Dr. Hannibal Lecter",            cohort: :november},
-           {name: "Darth Vader",                    cohort: :november},
-           {name: "Nurse Ratched",                  cohort: :november},
-           {name: "Michael Corleone",               cohort: :november},
-           {name: "Alex DeLarge",                   cohort: :november},
-           {name: "The Wicked Witch of the West",   cohort: :november},
-           {name: "Terminator",                     cohort: :november},
-           {name: "Freddie Kruger",                 cohort: :november},
-           {name: "The Joker",                      cohort: :november},
-           {name: "Joffrey Baratheon",              cohort: :november},
-           {name: "Norman Bates",                   cohort: :november}
-         ]
+#This method asks the user for input and adds it to the student array.
+def input_students
+   puts "Hello, and welcome to Villains Academy"
+   puts "Please enter the names of all the Villains who will be enrolling this semester"
+   puts "When you've entered each name, press Enter. To finish, enter a blank line"
+   
+   students=[]
+   
+   name=gets.chomp
+   
+    while !name.empty? do
+      students << {name: name, cohort: :november}
+        if students.length==1
+            puts "We now have 1 student enrolled."
+        else
+            puts "We now have #{students.length} students enrolled."
+        end
+      name=gets.chomp
+    end
+   
+   students
+end
 
 # This string covers 2 lines when putsed, saving space. 
 def put_header
     puts "The students of Villains Academy\n-----------------"
 end
 
-# This method has a parameter placeholder name that is then passed to the iterator
-# that takes each element in whatever array is passed to it, assigns it to the
-# placeholder name, then it executes the code block between do and end, which
-# in this case puts it to the screen.
-def puts_students name
-    name.each do |name|
-      puts "#{name[:name]} (#{name[:cohort]} cohort)"
+# This method takes the information passed to it (the student array), and passes it
+# into the each_with_Index iterator, which gives a way to access the array index info
+# within the iterator. Not possible to do this otherwise. Each entry in the array
+# is then accessed and has it's position + 1, name, and cohort key and value added
+# into the string, which is then putsed.
+def puts_students(student_list)
+    student_list.each_with_index do |name,name_index|
+      puts "#{name_index+1}. #{name[:name]}, (#{name[:cohort]} cohort)"
     end
+end
 
 def put_footer student
     puts "Overall we have #{student.count} great students."
 end
 
+
+# This is the input method
+students=input_students
 # Output methods called below
 put_header
 puts_students students
 put_footer students
+
+
+
+# Code below saved for possible later use
+# {name: "Dr. Hannibal Lecter",            cohort: :november},
+#           {name: "Darth Vader",                    cohort: :november},
+#           {name: "Nurse Ratched",                  cohort: :november},
+#           {name: "Michael Corleone",               cohort: :november},
+#           {name: "Alex DeLarge",                   cohort: :november},
+#           {name: "The Wicked Witch of the West",   cohort: :november},
+#           {name: "Terminator",                     cohort: :november},
+#           {name: "Freddie Kruger",                 cohort: :november},
+#           {name: "The Joker",                      cohort: :november},
+#           {name: "Joffrey Baratheon",              cohort: :november},
+#           {name: "Norman Bates",                   cohort: :november}
