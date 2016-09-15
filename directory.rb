4047e9baebf8d5e7f85cbfc783d9af976f01191d
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-15 09:17:06 +0000

    abandoned attempt to include 'print students beginning with letter ..' function with the while loop

--- directory.rb
+++ directory.rb
@@ -1,26 +1,38 @@
 #This method asks the user for input and adds it to the student array.
-def input_students
-   puts "Hello, and welcome to Villains Academy"
-   puts "Please enter the names of all the Villains who will be enrolling this semester"
-   puts "When you've entered each name, press Enter. To finish, enter a blank line"
+# def input_students
+#   puts "Hello, and welcome to Villains Academy"
+#   puts "Please enter the names of all the Villains who will be enrolling this semester"
+#   puts "When you've entered each name, press Enter. To finish, enter a blank line"
    
-   students=[]
+#   students=[]
    
-   name=gets.chomp
+#   name=gets.chomp
    
-    while !name.empty? do
-      students << {name: name, cohort: :november}
-        if students.length==1
-            puts "We now have 1 student enrolled."
-        else
-            puts "We now have #{students.length} students enrolled."
-        end
-      name=gets.chomp
-    end
+#     while !name.empty? do
+#       students << {name: :name, cohort: :november}
+#         if students.length==1
+#             puts "We now have 1 student enrolled."
+#         else
+#             puts "We now have #{students.length} students enrolled."
+#         end
+#       name=gets.chomp
+#     end
    
-   students
-end
+#   students
+# end
 
+students= [{name: "Dr. Hannibal Lecter",            cohort: :november},
+           {name: "Darth Vader",                    cohort: :november},
+           {name: "Nurse Ratched",                  cohort: :november},
+           {name: "Michael Corleone",               cohort: :november},
+           {name: "Alex DeLarge",                   cohort: :november},
+           {name: "The Wicked Witch of the West",   cohort: :november},
+           {name: "Terminator",                     cohort: :november},
+           {name: "Freddie Kruger",                 cohort: :november},
+           {name: "The Joker",                      cohort: :november},
+           {name: "Joffrey Baratheon",              cohort: :november},
+           {name: "Norman Bates",                   cohort: :november}
+           ]
 # This string covers 2 lines when putsed, saving space. 
 def put_header
     puts "The students of Villains Academy\n-----------------"
@@ -31,14 +43,26 @@ end
 # within the iterator. Not possible to do this otherwise. Each entry in the array
 # is then accessed and has it's position + 1, name, and cohort key and value added
 # into the string, which is then putsed.
+
 def puts_students(student_list)
     puts "Print students beginning with the letter:"
-    letter=gets.chomp
-    student_list.each_with_index do |name,name_index|
-        if name[:name][0].downcase==letter.downcase && name[:name].length<12
-            puts "#{name_index+1}. #{name[:name]}, (#{name[:cohort]} cohort.)"
+    test_letter=gets.chomp
+    puts "test letter is #{test_letter}"
+    # if test_letter=""
+    #     test_letter=student_list[0][:name][0]
+    # end
+    student_letter=student_list[0][:name][0]
+    puts "student letter is #{student_letter}"
+    counter=0
+    
+    while test_letter.downcase==student_letter.downcase do
+    #&& student_list[:name].to_i.length<12 
+            puts "#{student_list[counter][0]}. #{student_list[counter][:name]}, (#{student_list[counter][:cohort]} cohort.)"
+            counter=counter+1
+            until counter>student_list.length
         end
-    end
+    end   
+    
 end
 
 def put_footer student
@@ -47,7 +71,7 @@ end
 
 
 # This is the input method
-students=input_students
+# students=input_students
 # Output methods called below
 put_header
 puts_students students
@@ -56,14 +80,3 @@ put_footer students
 
 
 # Code below saved for possible later use
-# {name: "Dr. Hannibal Lecter",            cohort: :november},
-#           {name: "Darth Vader",                    cohort: :november},
-#           {name: "Nurse Ratched",                  cohort: :november},
-#           {name: "Michael Corleone",               cohort: :november},
-#           {name: "Alex DeLarge",                   cohort: :november},
-#           {name: "The Wicked Witch of the West",   cohort: :november},
-#           {name: "Terminator",                     cohort: :november},
-#           {name: "Freddie Kruger",                 cohort: :november},
-#           {name: "The Joker",                      cohort: :november},
-#           {name: "Joffrey Baratheon",              cohort: :november},
-#           {name: "Norman Bates",                   cohort: :november}
