05ae95f50efd2275548a69b03350a92cec0342a8
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-15 10:26:39 +0000

    finished pg.8 exercise 4 while loop

--- directory.rb
+++ directory.rb
@@ -1,38 +1,26 @@
 #This method asks the user for input and adds it to the student array.
-# def input_students
-#   puts "Hello, and welcome to Villains Academy"
-#   puts "Please enter the names of all the Villains who will be enrolling this semester"
-#   puts "When you've entered each name, press Enter. To finish, enter a blank line"
+def input_students
+  puts "Hello, and welcome to Villains Academy"
+  puts "Please enter the names of all the Villains who will be enrolling this semester"
+  puts "When you've entered each name, press Enter. To finish, enter a blank line"
    
-#   students=[]
+  students=[]
    
-#   name=gets.chomp
+  name=gets.chomp
    
-#     while !name.empty? do
-#       students << {name: :name, cohort: :november}
-#         if students.length==1
-#             puts "We now have 1 student enrolled."
-#         else
-#             puts "We now have #{students.length} students enrolled."
-#         end
-#       name=gets.chomp
-#     end
-   
-#   students
-# end
+    while !name.empty? do
+      students << {name: name, cohort: :november}
+        if students.length==1
+            puts "We now have 1 student enrolled."
+        else
+            puts "We now have #{students.length} students enrolled."
+        end
+      name=gets.chomp
+    end
+  students
+end
 
-students= [{name: "Dr. Hannibal Lecter",            cohort: :november},
-           {name: "Darth Vader",                    cohort: :november},
-           {name: "Nurse Ratched",                  cohort: :november},
-           {name: "Michael Corleone",               cohort: :november},
-           {name: "Alex DeLarge",                   cohort: :november},
-           {name: "The Wicked Witch of the West",   cohort: :november},
-           {name: "Terminator",                     cohort: :november},
-           {name: "Freddie Kruger",                 cohort: :november},
-           {name: "The Joker",                      cohort: :november},
-           {name: "Joffrey Baratheon",              cohort: :november},
-           {name: "Norman Bates",                   cohort: :november}
-           ]
+
 # This string covers 2 lines when putsed, saving space. 
 def put_header
     puts "The students of Villains Academy\n-----------------"
@@ -45,22 +33,11 @@ end
 # into the string, which is then putsed.
 
 def puts_students(student_list)
-    puts "Print students beginning with the letter:"
-    test_letter=gets.chomp
-    puts "test letter is #{test_letter}"
-    # if test_letter=""
-    #     test_letter=student_list[0][:name][0]
-    # end
-    student_letter=student_list[0][:name][0]
-    puts "student letter is #{student_letter}"
     counter=0
     
-    while test_letter.downcase==student_letter.downcase do
-    #&& student_list[:name].to_i.length<12 
-            puts "#{student_list[counter][0]}. #{student_list[counter][:name]}, (#{student_list[counter][:cohort]} cohort.)"
-            counter=counter+1
-            until counter>student_list.length
-        end
+    while counter < student_list.length do
+        puts "#{counter+1}. #{student_list[counter][:name]}, (#{student_list[counter][:cohort]} cohort)"    
+        counter=counter+1
     end   
     
 end
@@ -71,7 +48,7 @@ end
 
 
 # This is the input method
-# students=input_students
+students=input_students
 # Output methods called below
 put_header
 puts_students students
@@ -80,3 +57,17 @@ put_footer students
 
 
 # Code below saved for possible later use
+#students= [{name: "Dr. Hannibal Lecter",            cohort: :november},
+#           {name: "Darth Vader",                    cohort: :november},
+#           {name: "Nurse Ratched",                  cohort: :november},
+#           {name: "Michael Corleone",               cohort: :november},
+#           {name: "Alex DeLarge",                   cohort: :november},
+#           {name: "The Wicked Witch of the West",   cohort: :november},
+#           {name: "Terminator",                     cohort: :november},
+#           {name: "Freddie Kruger",                 cohort: :november},
+#           {name: "The Joker",                      cohort: :november},
+#           {name: "Joffrey Baratheon",              cohort: :november},
+#           {name: "Norman Bates",                   cohort: :november}
+#           ]
+
+            # puts "#{student_list[counter][0]}. #{student_list[0][0]}, (#{student_list[0][:cohort]} cohort)"
---
+++ directory_while_loop_first_letter.rb
@@ -0,0 +1,82 @@
+#This method asks the user for input and adds it to the student array.
+# def input_students
+#   puts "Hello, and welcome to Villains Academy"
+#   puts "Please enter the names of all the Villains who will be enrolling this semester"
+#   puts "When you've entered each name, press Enter. To finish, enter a blank line"
+   
+#   students=[]
+   
+#   name=gets.chomp
+   
+#     while !name.empty? do
+#       students << {name: :name, cohort: :november}
+#         if students.length==1
+#             puts "We now have 1 student enrolled."
+#         else
+#             puts "We now have #{students.length} students enrolled."
+#         end
+#       name=gets.chomp
+#     end
+   
+#   students
+# end
+
+students= [{name: "Dr. Hannibal Lecter",            cohort: :november},
+           {name: "Darth Vader",                    cohort: :november},
+           {name: "Nurse Ratched",                  cohort: :november},
+           {name: "Michael Corleone",               cohort: :november},
+           {name: "Alex DeLarge",                   cohort: :november},
+           {name: "The Wicked Witch of the West",   cohort: :november},
+           {name: "Terminator",                     cohort: :november},
+           {name: "Freddie Kruger",                 cohort: :november},
+           {name: "The Joker",                      cohort: :november},
+           {name: "Joffrey Baratheon",              cohort: :november},
+           {name: "Norman Bates",                   cohort: :november}
+           ]
+# This string covers 2 lines when putsed, saving space. 
+def put_header
+    puts "The students of Villains Academy\n-----------------"
+end
+
+# This method takes the information passed to it (the student array), and passes it
+# into the each_with_Index iterator, which gives a way to access the array index info
+# within the iterator. Not possible to do this otherwise. Each entry in the array
+# is then accessed and has it's position + 1, name, and cohort key and value added
+# into the string, which is then putsed.
+
+def puts_students(student_list)
+    puts "Print students beginning with the letter:"
+    test_letter=gets.chomp
+    puts "test letter is #{test_letter}"
+    # if test_letter=""
+    #     test_letter=student_list[0][:name][0]
+    # end
+    student_letter=
+    puts "student letter is #{student_letter}"
+    counter=0
+    
+    while test_letter.downcase==student_list[counter][:name][0].downcase do
+    #&& student_list[:name].to_i.length<12 
+            puts "#{student_list[counter][0]}. #{student_list[counter][:name]}, (#{student_list[counter][:cohort]} cohort.)"
+            counter=counter+1
+            until counter>student_list.length
+        end
+    end   
+    
+end
+
+def put_footer student
+    puts "Overall we have #{student.count} great students."
+end
+
+
+# This is the input method
+# students=input_students
+# Output methods called below
+put_header
+puts_students students
+put_footer students
+
+
+
+# Code below saved for possible later use
